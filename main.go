6dcc0794a597870c1c600e7bb4942fb9dc61cc00
dps910 <dps141618@gmail.com>
2020-05-01 09:46:37 +0100

    JSON response from e621

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+var client = http.Client{
+	// to comply with e621 rate limits of 2 reqs per second
+	Timeout: 5 * time.Second,
+}
+
+func GetByMD5(hash string) {
+	// What the request will be
+	request, err := http.NewRequest("GET", "https://e621.net/posts.json?tags=md5:"+hash, nil)
+	if err != nil {
+		log.Println("Invalid md5 hash", err)
+	}
+
+	request.Header.Set("User-Agent", "made in Golang :3")
+
+	// Now to actually make the request, where a response will be returned
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println("Couldn't make http request", err)
+	}
+	defer response.Body.Close()
+
+	// ioutil used to read the response :D
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%s", body)
+}
+
+func main() {
+	GetByMD5("d91986beca5ca13d88b200109a412d24")
+}
