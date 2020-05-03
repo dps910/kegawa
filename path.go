808e1784a906d67b59d9a3d11faf75591806b261
dps910 <dps141618@gmail.com>
2020-05-03 23:23:10 +0100

    a

--- path.go
+++ path.go
@@ -8,6 +8,9 @@ import (
 	"os"
 )
 
+// List of species
+var species = []string{"lucario", "fox", "pokemon"}
+
 func main() {
 	// Read text file
 	file, err := os.Open("./file.txt")
@@ -17,22 +20,6 @@ func main() {
 
 	// Defer closing of file, because now the md5 checksum needs to be checked
 	defer file.Close()
-
-	// Read file contents
-	s := bufio.NewScanner(file)
-
-	// Reads text from source file "file.txt"
-	scantext := s.Scan()
-
-	for scantext {
-		// Reads text from the most recent call generated to Scan()
-		// From my understanding, s.Scan() stores the text in a buffer
-		// and now it can be read by calling s.Text()
-		log.Printf("Here is the text from the file: %s", s.Text())
-
-		// Return to stop it printing a million times
-		return
-	}
 
 	// Return new hash.Hash to compute the md5 checksum of file
 	hash := md5.New()
@@ -45,4 +32,26 @@ func main() {
 
 	// Get md5 checksum of the file data
 	log.Printf("md5 checksum: %x", hash.Sum(nil))
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		log.Println("oof", err)
+	}
+
+	// Read file contents
+	s := bufio.NewScanner(file)
+
+	for s.Scan() {
+		// Reads text from the most recent call generated to Scan()
+		// From my understanding, s.Scan() stores the text in a buffer
+		// and now it can be read by calling s.Text()
+		log.Printf("Here is the text from the file: %s", s.Text())
+
+		// Return to stop it printing a million times
+		return
+	}
+
+	// How to use
+	// flag.String("path", "specify path to file", "./path.go -path=<PATH>")
+	// flag.Parse()
 }
