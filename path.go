12378dcc32761a6819fb861214aac543089198dd
dps <dps141618@gmail.com>
2020-05-20 08:37:25 +0100

    Can get md5 of file with

--- path.go
+++ path.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+)
+
+type A struct {
+	P []P `json:"posts"`
+}
+
+type P struct {
+	ID int `json:"id"`
+}
+
+var (
+	URL = "https://e621.net/posts.json?tags=md5"
+	c   = &http.Client{}
 )
 
 func hash() {
@@ -19,6 +35,40 @@ func hash() {
 
 	md5hash := md5.Sum(f)
 	log.Printf("md5 checksum: %x", md5hash)
+
+	// Send GET request to URL and hash
+	// If request is successful, return URL+hash
+	// If not successful, error
+	urlAndMd5 := fmt.Sprintf("%s:%x", URL, md5hash)
+	req, err := http.NewRequest("GET", urlAndMd5, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	req.Header.Add("User-Agent", "kegawa")
+
+	response, err := c.Do(req)
+	if err != nil {
+		log.Println("Couldn't send HTTP request. This is not an md5 from e621")
+	}
+	// Defer closing of response body so it can still be read
+	defer response.Body.Close()
+
+	// Read response body
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println("Could not read response body", err)
+	}
+
+	abc := A{}
+	json.Unmarshal(body, &abc)
+
+	// Print this epic data
+	if len(abc.P) == 0 {
+		log.Println("request did not succeed because not e621 hash. Or maybe the server is down and the furries made afucky wucky UWU")
+	} else {
+		log.Printf("%s:%x", URL, md5hash)
+	}
 }
 
 func main() {
