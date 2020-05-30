9a91eaae8c312d06bf39ced440dbeeede1822ab3
dps910 <dps141618@gmail.com>
2020-05-30 11:55:10 +0100

    69 commits :3

--- path.go
+++ path.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"crypto/md5"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,21 +11,35 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+// A API struct
 type A struct {
 	P []P `json:"posts"`
 }
 
+// P Posts struct
 type P struct {
+	File F    `json:"file"`
 	Tags Tags `json:"tags"`
 }
 
+// F File struct
+type F struct {
+	Md5 string `json:"md5"`
+}
+
+// Tags struct
 type Tags struct {
+	Artist  []string `json:"artist"`
 	Species []string `json:"species"`
 }
 
 var (
+	// URL variable (why do I need to comment this?)
 	URL = "https://e621.net/posts.json?tags=md5"
 	c   = &http.Client{}
 )
@@ -50,7 +66,7 @@ func hash() {
 		log.Println(err)
 	}
 
-	req.Header.Add("User-Agent", "kegawa")
+	req.Header.Add("User-Agent", "kegawa UwU")
 
 	response, err := c.Do(req)
 	if err != nil {
@@ -72,10 +88,61 @@ func hash() {
 	if len(abc.P) == 0 {
 		log.Println("request did not succeed because not e621 hash.")
 	} else {
+		artist := abc.P[0].Tags.Artist
 		species := abc.P[0].Tags.Species
 		log.Printf("%s:%x", URL, md5hash)
+		log.Printf("Artist: %s", strings.Join(artist, ""))
 		log.Printf("Species: %s", strings.Join(species, ", "))
 	}
+
+	// Database
+	db, err := sql.Open("sqlite3", "./e621.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check if database is alive (and not dead) :)
+	ctx, timeout := context.WithTimeout(context.Background(), 30*time.Second)
+	defer timeout()
+
+	connection := "Success"
+	if err := db.PingContext(ctx); err != nil {
+		connection = "Nope"
+	}
+	log.Println(connection)
+
+	// Create a table in sqlite3 :D
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS e621 (id INTEGER PRIMARY KEY, artist TEXT NOT NULL, urlMd5 TEXT NOT NULL, species TEXT NOT NULL)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Insert data :3
+	_, err = db.Exec(
+		"INSERT INTO e621 (artist, urlMd5, species) VALUES ($1, $2, $3)",
+		strings.Join(abc.P[0].Tags.Artist, ""),
+		urlAndMd5,
+		strings.Join(abc.P[0].Tags.Species, ", "),
+	)
+	// i
+	// need
+	// to
+	// get
+	// this
+	// to
+	// 69
+	// insertions/deletions
+	// owo
+	// uwu
+	//fwef ew/
+	//f wewe fwef w
+	// fwefwe fw/
+	// fwefwefwef
+	//f wrugetrihgeoruger
+	// rh4895y64y8j94e5g
+	// gfhwe84g3hg3h4g80h08
+	// werjgierjogejog
+	// jerigejirgejiropg
 }
 
 func main() {
